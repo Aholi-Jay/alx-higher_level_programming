da6cba4f8d18a59d8588e0f4a1102e98d87e2b33
JOsephat Aholi <111060732+Aholi-Jay@users.noreply.github.com>
2022-12-02 00:47:58 +0300

    Who are you?

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-# 4-hidden_discover
+# 4-hidden_discovery.py
 
 if __name__ == "__main__":
     """Print all names defined by hidden_4 module."""
@@ -7,5 +7,5 @@ if __name__ == "__main__":
 
     names = dir(hidden_4)
     for name in names:
-        if name[:2] != "__"
+        if name[:2] != "__":
             print(name)
