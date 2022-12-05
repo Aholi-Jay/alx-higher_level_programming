7420bae72e73116a356cb95055a83bc8d2bec260
Aholi-Jay <111060732+Aholi-Jay@users.noreply.github.com>
2022-12-05 23:50:23 +0300

    Can you C me now?

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def no_c(my_string):
+    new_string = ""
+    for chr in my_string:
+        if chr != "c" and chr != "c":
+            new_string += chr
+    return new_string
+
+
+
